962ab2cd20f288b686e067192219867f0738449f
hs-err <1102280066@qq.com>
2017-07-20 10:51:15 +0800

    Create mlwl_config.php

---
+++ mlwl_config.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '123Abc.123Abc.');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ultrax`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'u0PdCdR994I7wfYcA3Kf16zec4i3Lb3aBeq1bfh0laFa96P37ag1D6y7c1K0l8of');
+define('UC_API', 'http://119.28.134.74/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
